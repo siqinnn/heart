45be9069d47d2d3083811002c1702dafa9b8e481
siqinnn <siqin@yusys.com.cn>
2021-08-04 09:06:02 +0800

    拖拽

--- src/views/vue.vue
+++ src/views/vue.vue
@@ -393,7 +393,35 @@
         </div>
 
       </div>
+      <div class="shijiu">
+        <p class="heiha">24.拖拽</p>
+        <div style="display:flex;flex-direction:row">
+          <div style="width: 200px;
+          height: 300px;border:1px solid gray">
+            <div v-for="item in stringData" draggable="true" @dragstart="draStart(item,'startweidu')" @dragend="draEnd">
+              <!-- <i style="color:#2C8CF0;float:left;padding-top:2px" class="el-icon-circle-check"></i> -->
+              <!-- <el-tooltip :content="item.name" disabled>
+                <p class="leftfontclass">{{item.age}}</p>
+              </el-tooltip> -->
+              <el-tag type="success">{{item.age}}</el-tag>
+            </div>
+          </div>
 
+          <div class="dimensionality-small overflowy" @dragover="drgOver('weidu')" style="width: 200px;
+          height: 300px;border:1px solid gray">
+            <div v-for="(item,index) in dimensionData">
+             
+              <el-tag type="warning"> {{item.age}}</el-tag>
+              <i class="el-icon-close
+                " @click="removelist(index)"></i>
+            </div>
+
+          </div>
+        </div>
+
+
+
+      </div>
     </div>
   </div>
 
@@ -418,6 +446,28 @@
 
     data() {
       return {
+        targNode: '',
+        dimensionData: [],
+        stringData: [
+          {
+            name: 'tom',
+            age: '拖拽1',
+            number: '13522222222',
+            size: '38',
+          },
+          {
+            name: 'tom',
+            age: '拖拽2',
+            number: '13522222222',
+            size: '38',
+          },
+          {
+            name: 'tom',
+            age: '拖拽3',
+            number: '13522222222',
+            size: '38',
+          },
+        ],
         maplist: [
           {
             name: 'tom',
@@ -615,14 +665,58 @@
     },
 
     methods: {
+      draEnd: function (event) {
+        this.$nextTick(() => {
+          if (this.targNode === 'weidu' && this.startNode === 'startweidu') {
+            let getNode = this.dragNode;
+            let obj = {
+              name: getNode.name,
+              age: getNode.age
+            }
+            // test
+            this.dimensionData.push(obj)
+          }
+          // if (this.targNode === 'zhibiao' && this.startNode === 'startzhibiao') {
+          //     let getNode = this.dragNode;
+          //     let obj = {
+          //         fieldDescribe: getNode.fieldDescribe,
+          //         fieldName: getNode.fieldName,
+          //         column: getNode.fieldName,
+          //         func: 'sum',
+          //     };
+          //     // test
+          //     this.indexData.push(obj)
+
+          // }
+          // if (this.targNode === 'shaixuan') {
+          //     this.childNeedcolumn = this.dragNode.fieldName;
+          //     this.switchStatus('add', true);
+          //     this.indexscreen()
+          // }
+          // this.getData()
+
+        })
+        // this.dragNode = event;
+      },
+      removelist: function (index) {
+        this.dimensionData.splice(index, 1)
+        // this.getData()
+      },
+      draStart: function (event, type) {
+        this.startNode = type
+        this.dragNode = event;
+      },
+      drgOver: function (type) {
+        this.targNode = type;
+      },
       mapclick() {
-        this.listadd=this.maplist.map((item) => ({
-          namee:item.name,
-          agee:item.age
+        this.listadd = this.maplist.map((item) => ({
+          namee: item.name,
+          agee: item.age
         }))
         console.log(this.listadd)
-        
-        console.log(this.listadd.slice(0,1))
+
+        console.log(this.listadd.slice(0, 1))
       },
       format(percentage) {
         if (percentage) {
